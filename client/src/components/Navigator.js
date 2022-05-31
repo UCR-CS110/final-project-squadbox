ec50cf0289e1c95796b574a2665d8cca5e3314d0
Ivan Carrillo <helloivancar@gmail.com>
2022-05-30 17:55:01 -0700

    Fix sign in for guest

--- client/src/components/Navigator.js
+++ client/src/components/Navigator.js
@@ -5,35 +5,18 @@ import {
 import NavLinks from './NavLinks';
 
 const Navigator = () => {
-  let isLoggedIn = false;
-  if(localStorage.getItem("username") !== "") { // User is signed in
-    return (
-      <div className="w-full shadow-sm flex justify-between items-center p-4 h-12">
+  return (
+    <div className="w-full shadow-sm flex justify-between items-center p-4 h-12">
 
-        <div>
-          <span><Link to="/">SquadBox</Link></span>
-        </div>
-
-        <div>
-          <b>{localStorage.getItem("username")}</b>
-        </div>
+      <div>
+        <span><Link to="/">SquadBox</Link></span>
       </div>
-    );
-  }
-  else {
-    return (
-      <div className="w-full shadow-sm flex justify-between items-center p-4 h-12">
 
-        <div>
-          <span><Link to="/">SquadBox</Link></span>
-        </div>
-
-        <div>
-          <NavLinks isLoggedIn={isLoggedIn} />
-        </div>
+      <div>
+        <NavLinks />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Navigator;
