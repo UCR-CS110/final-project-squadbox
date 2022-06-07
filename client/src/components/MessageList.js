a34651ea1b35baf17236546b275ce367d593d405
IvanBot00 <ivangit@maybot.cc>
2022-06-07 14:56:34 -0700

    Re-add delete buttons

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import DeleteButton from '../components/deleteButton';
 
 const MessageList = ({ messages }) => {
   return (
     <div >
       <h1 className="font-30">Messages</h1>
-      {messages.map(message => <div>{message.nickname} {message.message} {message.vote} {message.timestamp}</div>)}
+      {messages.map(message => <div>{message.nickname} {message.message} {message.vote} {message.timestamp} <DeleteButton message={message.message}/> </div>)}
       <br></br>
     </div>
   );
